e33fa9dfe275fbc6b37006feb1d594e029657002
Terashima <yamanogomora@gmail.com>
2019-02-07 10:38:05 +0900

    完了済みのToDoにチェックマークを付ける機能を実装

--- SimpleTodoList/ViewController.swift
+++ SimpleTodoList/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // ToDo を格納した配列
-    var todoList = [String]()
+    var todoList = [MyTodo]()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -20,8 +20,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         // 保存しているToDoの読み込み処理
         let userDefaults = UserDefaults.standard
-        if let storedTodoList = userDefaults.array(forKey: "todoList") as? [String] {
-            todoList.append(contentsOf: storedTodoList)
+        if let storedTodoList = userDefaults.object(forKey: "todoList") as? Data {
+            if let unarchiveTodoList = NSKeyedUnarchiver.unarchiveObject(with: storedTodoList) as? [MyTodo] {
+                todoList.append(contentsOf: unarchiveTodoList)
+            }
         }
     }
 
@@ -40,10 +42,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         
         // 行番号に合ったToDoのタイトルを取得
-        let todoTitle = todoList[indexPath.row]
+        let myTodo = todoList[indexPath.row]
+        
         // セルのラベルにToDoのタイトルをセット
-        cell.textLabel?.text = todoTitle
+        cell.textLabel?.text = myTodo.todoTitle
+        
+        // セルのチェックマーク状態をセット
+        if myTodo.todoDone {
+            // チェックあり
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.none
+        }
+        
         return cell
+    }
+    
+    // セルをタップした時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let myTodo = todoList[indexPath.row]
+        if myTodo.todoDone {
+            // 完了済みの場合は未完了に変更
+            myTodo.todoDone = false
+        } else {
+            // 未完了の場合は完了済みに変更
+            myTodo.todoDone = true
+        }
+        // セルの状態を保存
+        tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+        
+        // データ保存
+        // Data型にシリアライズする
+        let data: Data = NSKeyedArchiver.archivedData(withRootObject: todoList)
+        
+        // UserDefaultsに保存
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "todoList")
+        userDefaults.synchronize()
+        
     }
 
     @IBAction func tapAddButton(_ sender: Any) {
@@ -56,15 +92,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // OKボタンを追加
         let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
             if let textField = alertController.textFields?.first {
-                // ToDo 配列に入力値を挿入。先頭に挿入する
-                self.todoList.insert(textField.text!, at: 0)
+                // ToDo 配列に入力値を先頭に挿入する
+                let myTodo = MyTodo()
+                myTodo.todoTitle = textField.text!
+                self.todoList.insert(myTodo, at: 0)
                 
                 // テーブルに行が追加されたことをテーブルに通知
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableViewRowAnimation.right)
                 
                 // ToDo の保存処理
                 let userDefaults = UserDefaults.standard
-                userDefaults.set(self.todoList, forKey: "todoList")
+                
+                // Data型にシリアライズする
+                let data = NSKeyedArchiver.archivedData(withRootObject: self.todoList)
+                userDefaults.set(data, forKey: "todoList")
                 userDefaults.synchronize()
             }
         }
@@ -78,5 +119,34 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         present(alertController, animated: true, completion: nil)
     }
     
+}
+
+// 独自クラスをシリアライズする際には、NSObjectを継承し
+// NSCodingプロトコルに準拠する必要がある
+class MyTodo: NSObject, NSCoding {
+    // ToDoのタイトル
+    var todoTitle: String?
+    
+    // ToDoを完了したかどうかを表すフラグ
+    var todoDone: Bool = false
+    
+    // コンストラクタ
+    override init() {
+        
+    }
+    
+    // NSCodingプロトコルに宣言されているデシリアライズ処理
+    // デコード処理とも呼ばれる
+    required init?(coder aDecoder: NSCoder) {
+        todoTitle = aDecoder.decodeObject(forKey: "todoTitle") as? String
+        todoDone = aDecoder.decodeBool(forKey: "todoDone")
+    }
+    
+    // NSCodingプロトコルに宣言されているシリアライズ処理
+    // エンコード処理とも呼ばれる
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(todoTitle, forKey: "todoTitle")
+        aCoder.encode(todoDone, forKey: "todoDone")
+    }
 }
 
