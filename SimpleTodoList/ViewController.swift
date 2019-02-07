1839458b386af06c58050677f8317af2c2747671
Terashima <yamanogomora@gmail.com>
2019-02-07 09:47:20 +0900

    画面のレイアウトを基本動作を実装

--- SimpleTodoList/ViewController.swift
+++ SimpleTodoList/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    // ToDo を格納した配列
+    var todoList = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // ToDoの配列の長さを返却する
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Storyboardで指定したtodoCell識別子を利用して再利用可能なセルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        
+        // 行番号に合ったToDoのタイトルを取得
+        let todoTitle = todoList[indexPath.row]
+        // セルのラベルにToDoのタイトルをセット
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
 
-
+    @IBAction func tapAddButton(_ sender: Any) {
+        // アラートダイアログを生成
+        let alertController = UIAlertController(title: "TODO追加", message: "TODOを入力してください", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // テキストエリアを追加
+        alertController.addTextField(configurationHandler: nil)
+        
+        // OKボタンを追加
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in
+            if let textField = alertController.textFields?.first {
+                // ToDo 配列に入力値を挿入。先頭に挿入する
+                self.todoList.insert(textField.text!, at: 0)
+                
+                // テーブルに行が追加されたことをテーブルに通知
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableViewRowAnimation.right)
+            }
+        }
+        alertController.addAction(okAction)
+        
+        // CANCELボタンがタップされた時の処理
+        let cancenButton = UIAlertAction(title: "CANCEL", style: UIAlertActionStyle.cancel, handler: nil)
+        alertController.addAction(cancenButton)
+        
+        // アラートダイアログを表示
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
